e4fdce4459668335914658d8dc547d26fe21a2ad
Oren Ben-Kiki <oren@ben-kiki.org>
2018-12-09 20:37:34 +0200

    Fix copyright notice.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
-// Copyright (C) 2017 Oren Ben-Kiki <oren@ben-kiki.org>
+// Copyright (C) 2017,2018 Oren Ben-Kiki <oren@ben-kiki.org>
 //
 // This file is part of cargo-coverage-annotations.
 //
-// Digitalis is free software: you can redistribute it and/or modify it under
-// the terms of the GNU General Public License, version 3, as published by the
-// Free Software Foundation.
+// cargo-coverage-annotations is free software: you can redistribute it and/or
+// modify it under the terms of the GNU General Public License, version 3, as
+// published by the Free Software Foundation.
 //
-// Digitalis is distributed in the hope that it will be useful, but WITHOUT ANY
-// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-// details.
+// cargo-coverage-annotations is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
 //
 // You should have received a copy of the GNU General Public License along with
-// cargo-fmt. If not, see <http://www.gnu.org/licenses/>.
+// cargo-coverage-annotations. If not, see <http://www.gnu.org/licenses/>.
 
 //! Ensure annotations in code match actual coverage.
 
